b07923090a83aad002435b825a3609a4d2ce827f
Prashant <41165408+prashantjha09@users.noreply.github.com>
2024-11-03 17:34:10 +0530

    Time: 39 ms (17.84%) | Memory: 27.9 MB (86.03%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        if len(s) < 10:
+            return []
+        frequency = defaultdict(int)
+        output = set()
+        i = 0
+        j = 1
+        count = 0
+        subarray = ""
+        while i < j < len(s):
+            # print(i, j, subarray)
+            if i == 0 and j == 1:
+                subarray = s[i]+s[j]
+                count += 2
+                j += 1
+                continue
+            subarray += s[j]
+            count += 1
+            if count == 10:
+                frequency[subarray] += 1
+                if frequency[subarray] > 1:
+                    output.add(subarray)
+                subarray = subarray[1:]
+                count -= 1
+                # print(frequency)
+                # print(output)
+                i += 1
+            j += 1
+        return list(output)
